164a5dea5cebc2daa5bbf57941a991198092e40d
Brandon <brandon.shiwsankar@gmail.com>
2023-04-17 18:40:46 -0400

    db connection

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 //Development configuration options
 module.exports = {
-  db: "mongodb://127.0.0.1:27017/comp308-project",
+  db: "mongodb://localhost/comp308-project",
   sessionSecret: "developmentSessionSecret"
 };
